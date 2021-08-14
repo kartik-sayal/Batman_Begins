30e57b35c42d92738600812b813655a5e469ce91
kartik-sayal <73281831+kartik-sayal@users.noreply.github.com>
2021-08-14 15:50:54 +0530

    Add files via upload

---
+++ Drops.js
@@ -0,0 +1,35 @@
+class createDrops{
+    constructor(x, y){
+        var options = { 
+
+            'restitution':0.1,
+            'friction':0.1,
+
+          }
+
+          this.body = Bodies.circle(x, y, 5, options);
+          this.radius = 3;
+
+          World.add(world, this.body);
+
+    }
+   
+    update(){
+
+      if(this.body.position.y > height){
+        Matter.Body.setPosition(this.body,{x:random(0, 500), y:random(0, 650)});
+
+      }
+
+    }
+  
+    display(){
+
+      noStroke();
+      fill(135,206,235);
+      ellipseMode(RADIUS);
+      ellipse(this.body.position.x, this.body.position.y, this.radius, this.radius);
+      
+    }
+  }
+
